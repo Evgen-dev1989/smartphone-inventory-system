b108011c3c42641043c01b4e8b341f1de27953fa
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2026-03-12 16:49:40 +0200

    Update logic.py

--- src/logic.py
+++ src/logic.py
@@ -2,13 +2,16 @@ from models import Warehouse, Smartphone
 
 
 warhause = Warehouse()
-iphone_15 = Smartphone(brand = 'Iphone',model = 15 ,price = '500$',quantity = 7)
-iphone_14_pro = Smartphone(brand = 'Iphone',model = '14 pro' ,price = '350$', quantity = 3)
+iphone_15 = Smartphone(brand = 'iphone', model = 15 ,price = '500$',quantity = 7)
+iphone_14_pro = Smartphone(brand = 'iphone',model = '14 pro' ,price = '350$', quantity = 3)
+xiaomi = Smartphone(brand = 'xiaomi', model = 'Redmi Note 14', price = '221$',quantity = 5)
+samsung = Smartphone(brand = 'samsung', model = 'Galaxy ', price = '433$', quantity = 4)
 
 
 warhause.add_product(iphone_15)
 warhause.add_product(iphone_14_pro)
-
+warhause.add_product(xiaomi)
+warhause.add_product(samsung)
 def sell_item():
     pass
 
